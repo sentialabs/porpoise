95deed5e44f13484b3916e83bfeeb8bae56dcad2
Wessel van Heerde <wessel.van.heerde@sentia.com>
2017-08-29 11:10:19 +0200

    Attempt to fix generator

--- lib/generators/porpoise/install_generator.rb
+++ lib/generators/porpoise/install_generator.rb
@@ -6,7 +6,7 @@ module Porpoise
     class InstallGenerator < ActiveRecord::Generators::Base
       source_root File.expand_path("../templates", __FILE__)
 
-      migration_template "migration.rb", "db/migrate/porpoise_create_key_value_objects.rb"
+      copy_file "migration.rb", "db/migrate/porpoise_create_key_value_objects.rb"
     end
   end
 end
