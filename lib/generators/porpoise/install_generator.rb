81b3d5a663f02748c2fa1924b67093a2bdd0beab
Wessel van Heerde <wessel.van.heerde@sentia.com>
2017-08-29 11:57:16 +0200

    Add date to migration file

--- lib/generators/porpoise/install_generator.rb
+++ lib/generators/porpoise/install_generator.rb
@@ -6,7 +6,7 @@ module Porpoise
 
     desc "Install database migration file"
     def create_migration_file
-      copy_file "migration.rb", "db/migrate/porpoise_create_key_value_objects.rb"
+      copy_file "migration.rb", "db/migrate/#{Date.today.strftime('%Y%m%d%H%M%S')}_porpoise_create_key_value_objects.rb"
     end
   end
 end
