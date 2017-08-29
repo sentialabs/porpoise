e965606f63b1efcb13edda3d5bbc3236c03aec1a
Wessel van Heerde <wessel.van.heerde@sentia.com>
2017-08-29 11:53:24 +0200

    Attempt to fix generator

--- lib/generators/porpoise/install_generator.rb
+++ lib/generators/porpoise/install_generator.rb
@@ -2,10 +2,11 @@ require 'rails/generators'
 
 module Porpoise
   class InstallGenerator < Rails::Generators::Base
-    desc "Install database migration file"
-
     source_root File.expand_path("../templates", __FILE__)
 
-    copy_file "migration.rb", "db/migrate/porpoise_create_key_value_objects.rb"
+    desc "Install database migration file"
+    def create_migration_file
+      copy_file "migration.rb", "db/migrate/porpoise_create_key_value_objects.rb"
+    end
   end
 end
