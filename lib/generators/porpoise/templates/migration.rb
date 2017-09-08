46cded654787185ab38a67dfeef08fa524c371a7
Wessel van Heerde <wessel.van.heerde@sentia.com>
2017-09-08 09:13:45 +0200

    Attempt to fix migration table issue

--- lib/generators/porpoise/templates/migration.rb
+++ lib/generators/porpoise/templates/migration.rb
@@ -1,19 +1,15 @@
 class PorpoiseCreateKeyValueObjects < ActiveRecord::Migration
-  def connection
-    ActiveRecord::Base.establish_connection("porpoise_#{Rails.env}").connection
-  end
-
   def change
-    create_table :key_value_objects, id: false do |t|
+    KeyValueObject.connection.create_table :key_value_objects, id: false do |t|
       t.string :key, null: false
       t.string :data_type, null: false, limit: 10
       t.text :value, limit: 65000000, null: false
       t.datetime :expiration_date
     end
 
-    add_index :key_value_objects, :key, unique: true
-    add_index :key_value_objects, :key, type: :fulltext
-    add_index :key_value_objects, :data_type
-    add_index :key_value_objects, :expiration_date
+    KeyValueObject.connection.add_index :key_value_objects, :key, unique: true
+    KeyValueObject.connection.add_index :key_value_objects, :key, name: 'key_fulltext_idx', type: :fulltext
+    KeyValueObject.connection.add_index :key_value_objects, :data_type
+    KeyValueObject.connection.add_index :key_value_objects, :expiration_date
   end
 end
