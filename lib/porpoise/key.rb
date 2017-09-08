773f6b4c3332ff0ce0c8d129c5cd69e2c225e87e
Wessel van Heerde <wessel.van.heerde@sentia.com>
2017-09-08 15:53:45 +0200

    Fix matcher

--- lib/porpoise/key.rb
+++ lib/porpoise/key.rb
@@ -66,7 +66,7 @@ module Porpoise
 
       def keys(key_name_or_search_string)
         if key_name_or_search_string.include?('*')
-          return Porpoise::KeyValueObject.where(['key LIKE ?', key_name_or_search_string.gsub('%', '*')]).pluck(:key)
+          return Porpoise::KeyValueObject.where(['`key` LIKE ?', key_name_or_search_string.gsub('*', '%')]).pluck(:key)
         else
           return Porpoise::KeyValueObject.where(key: key_name_or_search_string).pluck(:key)
         end
