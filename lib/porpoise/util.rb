8821d180aca78961b5fa2f3c5d828b1306413ba2
Wessel van Heerde <wessel.van.heerde@sentia.com>
2017-09-08 15:19:54 +0200

    Fix caching

--- lib/porpoise/util.rb
+++ lib/porpoise/util.rb
@@ -2,7 +2,7 @@ module Porpoise
   module Util
     class << self
       def ping
-        return !Porpoise::KeyValueObject.last.blank?
+        return Porpoise::KeyValueObject.count >= 0
       end
     end
   end
